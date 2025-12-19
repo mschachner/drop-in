5067247651015887c1ab362a0779c3e76c1d47cc
Mark Schachner <markj.schachner@gmail.com>
2025-12-19 12:08:19 -0500

    Allow custom domain in CORS

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ const app = express();
 
 // Enable CORS with proper preflight handling
 app.use(cors({
-  origin: ['https://drop-in.up.railway.app', 'http://localhost:3000'],
+  origin: [
+    'https://drop-in.up.railway.app',
+    'https://www.drop-in-cal.com',
+    'http://localhost:3000'
+  ],
   methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
